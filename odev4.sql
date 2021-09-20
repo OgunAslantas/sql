71d07c6f8b5fea8cba343a6355bb98259aa354e5
ogün <ogun2011@gmail.com>
2021-09-20 12:48:27 +0300

    Create odev4.sql

---
+++ odev4.sql
@@ -0,0 +1,12 @@
+
+-- ****** ÖDEV-4 ******
+
+--SELECT DISTINCT replacement_cost FROM film;
+
+--SELECT COUNT(DISTINCT replacement_cost) FROM film;
+
+--SELECT COUNT(title LIKE 'T%' AND rating = 'G') FROM film;
+
+--SELECT COUNT(country_id = 5) FROM country;
+
+--SELECT COUNT(city ILIKE '%r') FROM city;
