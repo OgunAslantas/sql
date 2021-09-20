816bb4ecaa0f53eab357c01df2c76a83114f57c1
ogün <ogun2011@gmail.com>
2021-09-20 12:31:27 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,19 @@
+
+-- ****** ÖDEV-3 ******
+
+--SELECT country FROM country
+--WHERE country LIKE 'A%a';
+
+--SELECT country FROM country
+--WHERE country LIKE '%n' AND country_id > 6 ;
+
+--SELECT title FROM film
+--WHERE title ILIKE '%t';
+
+--SELECT title FROM film
+--WHERE title LIKE 'C&' AND length >90 AND rental_rate = 2.99;
+
+
+
+
+
